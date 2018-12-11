728196e085304913eee676a42433f88859205161
sylvainpolletvillard <sylvain.pollet.villard@gmail.com>
2018-12-11 23:47:35 +0100

    essais effets audio dégueulasses

--- src/game/utils/audio.js
+++ src/game/utils/audio.js
@@ -2,8 +2,7 @@ export const sounds = {}
 
 export function loadAudio() {
     game.load.audio("music", [
-        "assets/sound/ancient_path.ogg",
-        "assets/sound/ancient_path.mp3"
+        "assets/sound/ForestV1.mp3"
     ]);
     game.load.audio("hurt", ["assets/sound/hurt.ogg", "assets/sound/hurt.mp3"]);
     game.load.audio("slash", [
@@ -29,6 +28,28 @@ export function addSounds() {
 export function startMusic() {
     game.music = game.add.audio("music");
     game.music.loop = true;
-    //game.music.play();
+    game.music.play();
+
+    game.music.onPlay.add(() => addEffects());
     game.onExit = () => game.music.stop();
+}
+
+export function addEffects() {
+    const context = game.music.context;
+    const source = game.music._sound;
+    source.disconnect(0);
+    source.playbackRate.value = 1.2;
+
+
+    const tuna = new Tuna(context);
+    const effect = new tuna.Overdrive({
+        outputGain: 0.25,         //0 to 1+
+        drive: 0.3,              //0 to 1
+        curveAmount: 1,          //0 to 1
+        algorithmIndex: 0,       //0 to 5, selects one of our drive algorithms
+        bypass: 0
+    });
+
+    source.connect(effect);
+    effect.connect(game.music.externalNode || game.music.gainNode);
 }
