507917273c5958d2eaaa006b829cd9fb268f6b42
Boris Couturier <boris.couturier@santeos.com>
2019-02-12 18:28:31 +0100

    Feat(Escape) : Ajout de l'élément Chalet pour lancer le mini jeu

--- src/game/scenes/LoadingScene.js
+++ src/game/scenes/LoadingScene.js
@@ -53,7 +53,7 @@ export class LoadingScene {
 
 		// images
 		game.load.image("exit", "assets/environment/exit-open.png");
-		game.load.image("arbre", "assets/escape_game/chalet.png");
+		game.load.image("chalet", "assets/escape/chalet.png");
 
 		// audio
 		loadAudio();
--- src/game/scenes/minigames/EscapeGame.js
+++ src/game/scenes/minigames/EscapeGame.js
@@ -1,14 +1,23 @@
 import { Game } from "phaser-ce";
-
+import { loadSave } from "../../save";
 
 export class EscapeGameScene {
     preload() {
-        game.load.image('etablie', 'assets/escape_game/etablie.png');
+        game.load.image('etablie', 'assets/escape/etablie.png');
     }
 
     create() {
         game.scale.setGameSize(800, 450);
+        game.add.image(0, 0, "etablie");
+
+        const startKey = game.input.keyboard.addKey(Phaser.Keyboard.ESC);
+        startKey.onDown.add(this.quitGame, this);
         // game.add.sprite(0, 0, 'etablie');
+    }
+
+    quitGame() {
+        loadSave();
+        game.state.start('MainGame');
     }
 
     update() {
