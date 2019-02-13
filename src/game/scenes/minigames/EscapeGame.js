3888be93e32670fd26f97a9477a60a9ac18523cb
Boris Couturier <boris.couturier@santeos.com>
2019-02-13 18:29:57 +0100

    FEAT(Escape) Ajout d'éléments

--- src/game/scenes/minigames/EscapeGame.js
+++ src/game/scenes/minigames/EscapeGame.js
@@ -4,23 +4,29 @@ import { loadSave } from "../../save";
 export class EscapeGameScene {
     preload() {
         game.load.image('etablie', 'assets/escape/etablie.png');
+        game.load.image('potfleur', 'assets/escape/potfleur1.png');
+        game.load.image('digicode_boite', 'assets/escape/digicode_boite.png');
+        game.load.spritesheet('digicode_ledbot', 'assets/escape/digicode_ledbot.png', 4, 5, 2);
     }
 
     create() {
         game.scale.setGameSize(800, 450);
+        
         game.add.image(0, 0, "etablie");
+        game.add.image(100, 200, "potfleur");
+        game.add.image(495, 342, "digicode_boite");
+        game.add.image(498, 383, "digicode_ledbot", 1);
 
         const startKey = game.input.keyboard.addKey(Phaser.Keyboard.ESC);
         startKey.onDown.add(this.quitGame, this);
-        // game.add.sprite(0, 0, 'etablie');
+    }
+
+    update() {
+
     }
 
     quitGame() {
         loadSave();
         game.state.start('MainGame');
-    }
-
-    update() {
-
     }
 }
