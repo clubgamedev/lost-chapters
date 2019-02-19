dd6888ed8d8bf7a914c65272c9e164307ede1f2e
Myzuno <myzuno@macbook-pro-de-myzuno.home>
2019-02-20 00:56:42 +0100

    FEAT(Escape): Premiere interaction avec un bouton

--- src/game/scenes/minigames/EscapeGame.js
+++ src/game/scenes/minigames/EscapeGame.js
@@ -11,15 +11,15 @@ export class EscapeGameScene {
         game.load.spritesheet('digicode_cable', 'assets/escape/digicode/digicode_cable.png', 3, 19, 2);
         game.load.spritesheet('digicode_ledbot', 'assets/escape/digicode/digicode_ledbot.png', 4, 5, 2);
         game.load.spritesheet('digicode_ledonoff', 'assets/escape/digicode/digicode_ledonoff.png', 4, 5, 2);
-        game.load.spritesheet('digicode_btn1', 'assets/escape/digicode/digicode_btn1.png', 6, 8, 2);
-        game.load.spritesheet('digicode_btn2', 'assets/escape/digicode/digicode_btn2.png', 6, 8, 2);
-        game.load.spritesheet('digicode_btn3', 'assets/escape/digicode/digicode_btn3.png', 6, 8, 2);
-        game.load.spritesheet('digicode_btn4', 'assets/escape/digicode/digicode_btn4.png', 6, 8, 2);
-        game.load.spritesheet('digicode_btn5', 'assets/escape/digicode/digicode_btn5.png', 6, 8, 2);
-        game.load.spritesheet('digicode_btn6', 'assets/escape/digicode/digicode_btn6.png', 6, 8, 2);
-        game.load.spritesheet('digicode_btn7', 'assets/escape/digicode/digicode_btn7.png', 6, 8, 2);
-        game.load.spritesheet('digicode_btn8', 'assets/escape/digicode/digicode_btn8.png', 6, 8, 2);
-        game.load.spritesheet('digicode_btn9', 'assets/escape/digicode/digicode_btn9.png', 6, 8, 2);
+        game.load.spritesheet('digicode_btn1', 'assets/escape/digicode/digicode_btn1.png', 5, 8, 2);
+        game.load.spritesheet('digicode_btn2', 'assets/escape/digicode/digicode_btn2.png', 5, 8, 2);
+        game.load.spritesheet('digicode_btn3', 'assets/escape/digicode/digicode_btn3.png', 5, 8, 2);
+        game.load.spritesheet('digicode_btn4', 'assets/escape/digicode/digicode_btn4.png', 5, 8, 2);
+        game.load.spritesheet('digicode_btn5', 'assets/escape/digicode/digicode_btn5.png', 5, 8, 2);
+        game.load.spritesheet('digicode_btn6', 'assets/escape/digicode/digicode_btn6.png', 5, 8, 2);
+        game.load.spritesheet('digicode_btn7', 'assets/escape/digicode/digicode_btn7.png', 5, 8, 2);
+        game.load.spritesheet('digicode_btn8', 'assets/escape/digicode/digicode_btn8.png', 5, 8, 2);
+        game.load.spritesheet('digicode_btn9', 'assets/escape/digicode/digicode_btn9.png', 5, 8, 2);
     }
 
     create() {
@@ -38,6 +38,11 @@ export class EscapeGameScene {
 
     }
 
+    _click(image) {
+        debugger;
+
+    }
+
     _createDigicode() {
         let DIGICODE_X = 495;
         let DIGICODE_Y = 342;
@@ -50,7 +55,13 @@ export class EscapeGameScene {
         game.add.image(DIGICODE_X + 27, DIGICODE_Y + 41, "digicode_ledbot", 0);
         game.add.image(DIGICODE_X + 3, DIGICODE_Y + 8, "digicode_ledonoff", 0);
 
-        game.add.image(DIGICODE_X + 10, DIGICODE_Y + 8, "digicode_btn1", 0);
+        // let btn1 = game.add.image(DIGICODE_X + 10, DIGICODE_Y + 8, "digicode_btn1", 0);
+        let btn1 = game.add.image(DIGICODE_X - 50, DIGICODE_Y - 50, "digicode_btn1", 0);
+        btn1.scale.setTo(10, 10);
+        btn1.inputEnabled = true;
+        btn1.events.onInputDown.add(() => {btn1.frame = 1;});
+        btn1.events.onInputUp.add(() => {btn1.frame = 0;});
+
         game.add.image(DIGICODE_X + 17, DIGICODE_Y + 8, "digicode_btn2", 0);
         game.add.image(DIGICODE_X + 24, DIGICODE_Y + 8, "digicode_btn3", 0);
         game.add.image(DIGICODE_X + 10, DIGICODE_Y + 19, "digicode_btn4", 0);
