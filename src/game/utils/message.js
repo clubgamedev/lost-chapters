1b48affc25110b67ea2823a60ab4030e4a6edd1b
sylvainpolletvillard <sylvain.pollet.villard@gmail.com>
2019-07-28 18:44:23 +0200

    fix middle text stacking

--- src/game/utils/message.js
+++ src/game/utils/message.js
@@ -1,4 +1,7 @@
 export function showMiddleText(textToDisplay, backgroundColor = 0x000000, textColor = "#FFFFFF", duration = 1500, textSize = "16px") {
+    if (game.middleTextTweens) {
+        Object.values(game.middleTextTweens).forEach(tween => tween.stop(true))
+    }
 
     let middleText = game.add.graphics(0, game.height / 3);
     middleText.fixedToCamera = true;
@@ -16,15 +19,19 @@ export function showMiddleText(textToDisplay, backgroundColor = 0x000000, textCo
     textSprite.setTextBounds(0, 0, middleText.width, middleText.height);
     middleText.addChild(textSprite);
 
-    let fadeIn = game.add.tween(middleText).to({alpha: 0.8}, 400, Phaser.Easing.Linear.None)
-    let textIn = game.add.tween(textSprite).to({x: 0}, 400, Phaser.Easing.Quadratic.InOut)
+    let fadeIn = game.add.tween(middleText).to({ alpha: 0.8 }, 400, Phaser.Easing.Linear.None)
+    let textIn = game.add.tween(textSprite).to({ x: 0 }, 400, Phaser.Easing.Quadratic.InOut)
     fadeIn.chain(textIn)
     fadeIn.start();
 
-    let textOut = game.add.tween(textSprite).to({x: game.width}, 400, Phaser.Easing.Quadratic.InOut)
-    let fadeOut = game.add.tween(middleText).to({alpha: 0}, 300, Phaser.Easing.Linear.None)
+    let textOut = game.add.tween(textSprite).to({ x: game.width }, 400, Phaser.Easing.Quadratic.InOut)
+    let fadeOut = game.add.tween(middleText).to({ alpha: 0 }, 300, Phaser.Easing.Linear.None)
     textOut.chain(fadeOut)
-    fadeOut.onComplete.addOnce(() => middleText.destroy())
+    game.middleTextTweens = { fadeIn, textIn, fadeOut, textOut };
+    fadeOut.onComplete.addOnce(() => {
+        middleText.destroy();
+        delete game.middleTextTweens;
+    })
 
     textIn.onComplete.addOnce(() => {
         setTimeout(() => textOut.start(), duration)
