63fcfc194c2e9e1a76631c5fad2182fef504ae1e
sylvainpolletvillard <sylvain.pollet.villard@gmail.com>
2019-07-28 18:39:39 +0200

    remove book and runes physic body

--- src/game/items/Book.js
+++ src/game/items/Book.js
@@ -2,10 +2,7 @@ export class Book extends Phaser.Sprite {
 
     constructor(position, properties) {
         super(game, position.x * 16, position.y * 16, "runes");
-        game.physics.arcade.enable(this);
         this.alpha = 0;
-        this.body.setSize(20, 20, -2, -2);
-        this.body.moves = false;
         this.type = "book";
         this.properties = properties;
     }
--- src/game/items/Runes.js
+++ src/game/items/Runes.js
@@ -2,9 +2,6 @@ export class Runes extends Phaser.Sprite {
 
     constructor(position, properties) {
         super(game, position.x * 16, position.y * 16, "runes");
-        game.physics.arcade.enable(this);
-        this.body.setSize(20, 20, -2, -2);
-        this.body.moves = false;
         this.type = "runes";
         this.properties = properties;
 
