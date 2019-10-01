cbb2379173a2d0f769726e2ec3c8c8b5fff4062d
sylvainpolletvillard <sylvain.pollet.villard@gmail.com>
2019-10-02 00:11:12 +0200

    WIP dialogues therled

--- src/game/dialogs/Therled.js
+++ src/game/dialogs/Therled.js
@@ -1,71 +1,70 @@
-export function Therled(save) {
-		if(save.EndRituelAndDialog){
-			return [
-				"Tu n'as pas ENTENDU ??",
-				"VA T-EN je dois me concentrer pour tout recommencer"
-				]
-		}
+import { talkToMyself } from "../utils/dialog"
 
-		if(save.hasBetrayedTherled){
-			return [
+export function therled(save) {
+	return [
+		"Ah enfin ! Donne-moi le parchemin, il est presque trop tard !",
+		{
+			"Donner le parchemin": () => [
+				"Bien, bien, nous allons pouvoir commencer...",
+				rituel1
+			],
+			"Refuser": () => [
+				"Qu'y a-t-il ? Tu abandonnes si près de notre but ?",
 				"Tu penses pouvoir empêcher la science d'avancer ?",
-				"Assez. Saisissez-le !"
+				"Tu sais le sort que l'on réserve aux traîtres...",
+				"Je te le redemande une dernière fois. Le parchemin.",
+				rituel1
 			]
 		}
-		
-		if(save.EndRituel){
-			save.EndRituelAndDialog =true;
-			if(save.SuccesRituel){
-				return[
-						"OUI OUI VIENS à moi mon chien",
-						"Tu es à moi désormais sois sous mon côntrole",
-						"Non comment est-ce possible, il me résiste à MOI, THERLED!",
-						"Non non non le sort va être rom....",
-						"ARGGG",
-						"fct(combatBossTindalos)"
-						]
-			}
-			return [
-					"Comment est-ce possible???",
-					"Me serais-je tromper.",
-					"Non non c'est ces livres qui ne sont pas correct!",
-					"Je vais devoir tout recommencer depuis le départ.",
-					"Faisss chier!",
-					".....",
-					"Les gars laisser moi seul."
-					]
-		}
-		
-		if(save.StartRituel){
-			return [
-				"Toi là bas va me chercher le parchemin d'incantation.",
-				"Oui Chef!",
-				" et toi rapporte moi la fiole de sang de tindalos.",
-				"A vos ordres patron",
-				"Maintenant le dernier ingrédient pour le rituel,",
-				"(mélange la drogue de Liao à la fiole de sang)",
-				" toi qui veut nous rejoindre prête nous allégence en buvant ça.",
-				{
-					"Boire la fiole": () => ["Bien Bien, ça va commencer",
-											 "(Vous poignarde)",
-											 "Ne t'inquiete pas tu va survivre à ce coup",
-											 "Et que maintant que le chien de tindalos se MONTRE!",
-											 "fct(ResultOfRituel)"
-											 ],
-					"Refuser": () => "fct(BetrayedTherled) Ben va te faire voir et meurs!"
-				}
-				]
-		}
-        if (save.hasMetTherled) {
-            return "Je ne souhaite pas être dérangé, retourne à ton poste !" ;
-        }
-        save.hasMetTherled = true;
-		return [
-			"Ahhh mais voiçi un candidat idéal pour être mon fidèle",
-			"Donne moi la drogue que l'on commence tout de suite",
-			{
-				"Donner la drogue": () => "Bien. Préparez le rituel.",
-				"Refuser": () => "Ben va te faire voir et meurs!"
-			}
-		]
+	]
+
 }
+
+function rituel1() {
+	return talkToMyself([
+		`Vous donnez le parchemin à Therled.`
+	]).then(() => [
+		`Maintenant, le sang pour appeler la bête...`,
+		`et le Liao pour voir à travers le voile...`,
+		rituel2
+	])
+}
+
+function rituel2() {
+	return talkToMyself([
+		`Therled mélange la drogue de Liao dans de petites fioles`,
+		`au contenu rouge sombre. On dirait du sang...`,
+		`Il distribue les fioles à ses compères, et vous en tend une,`,
+		`puis boit la dernière d'un cul sec. Les autres l'imitent.`
+	]).then(() => {
+		return {
+			"Boire la fiole": () => ["Et que maintant que le chien de tindalos se MONTRE!"],
+			"Boire la fausse fiole": () => ["Et que maintant que le chien de tindalos se MONTRE!"],
+		}
+	})
+}
+
+/*
+if (save.EndRituel) {
+	save.EndRituelAndDialog = true;
+	if (save.SuccesRituel) {
+		return [
+			"OUI OUI VIENS à moi mon chien",
+			"Tu es à moi désormais sois sous mon côntrole",
+			"Non comment est-ce possible, il me résiste à MOI, THERLED!",
+			"Non non non le sort va être rom....",
+			"ARGGG",
+			"fct(combatBossTindalos)"
+		]
+	}
+	return [
+		"Comment est-ce possible???",
+		"Me serais-je tromper.",
+		"Non non c'est ces livres qui ne sont pas correct!",
+		"Je vais devoir tout recommencer depuis le départ.",
+		"Faisss chier!",
+		".....",
+		"Les gars laisser moi seul."
+	]
+
+}*/
