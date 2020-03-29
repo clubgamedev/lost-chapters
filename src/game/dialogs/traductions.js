7b49b5236823663096b9c4febafb0087e63a1e2d
sylvainpolletvillard <sylvain.pollet.villard@gmail.com>
2020-03-29 17:48:17 +0200

    runes foret et terrier

--- src/game/dialogs/traductions.js
+++ src/game/dialogs/traductions.js
@@ -3,24 +3,24 @@ import { talkToMyself } from "../utils/dialog"
 export const traductions = {
     traduction_tableau: {
         lines: [`Que tout le monde se rassemble au terrier`],
-        after(save){
+        after(save) {
             save.hasDiscoveredTerrier = true
         }
-	},
+    },
 
-	traduction_souche: {
+    traduction_souche: {
         lines: [
-		`Ne passez plus par le pont de la rivière.`,
-        `Le vieux Ramsey devient un peu trop curieux...`
+            `Ne passez plus par le pont de la rivière.`,
+            `Le vieux Ramsey devient un peu trop curieux...`
         ]
     },
 
-	traduction_camp_ramsey: {
+    traduction_camp_ramsey: {
         lines: [
             `Nous nous sommes occupés de Ramsey.`,
-		    `Rassemblez-vous au terrier pour le rituel.`
+            `Rassemblez-vous au terrier pour le rituel.`
         ],
-        after(save){
+        after(save) {
             save.canUseChaudron = true;
             return talkToMyself([
                 `Qui a pu kidnapper Ramsey ? Les dévots ? Les étudiants ?`,
@@ -31,6 +31,33 @@ export const traductions = {
                 `Je vais devoir me débrouiller seul pour préparer l'antidote au Liao.`,
             ])
         }
-    }
+    },
 
+    traduction_aide_foret: {
+        lines: [
+            `Si vous ne retrouvez pas le chemin du terrier,`,
+            `suivez les traces de nos capes dans la boue.`
+        ]
+    },
+
+    traduction_entree_grotte: {
+        lines: [
+            `Ne laissez personne pénétrer dans le Terrier.`,
+            `Utilisez les runes pour les neutraliser si nécessaire.`
+        ],
+        after(save) {
+            if (save.hasBeatenArthur) {
+                return talkToMyself([
+                    `Utiliser les runes... Comme ce que le père Arthur m'a fait ?`,
+                    `C'est plus qu'une langue, c'est un moyen de manipuler les esprits !`,
+                    `Je ferais bien d'avancer discrètement ici...`
+                ])
+            } else {
+                return talkToMyself([
+                    `Utiliser les runes ? Qu'est-ce que ça peut vouloir dire ?`,
+                    `Je ferais bien d'avancer discrètement ici...`
+                ])
+            }
+        }
+    }
 }
